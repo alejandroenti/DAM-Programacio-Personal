d06ac41cf205a5c473086b71845baf716c8d7e92
Alejandro López <programming.shrine454@slmails.com>
2024-10-15 19:20:45 +0200

    Impimir tablero e información
    Realizando la impresión del tablero con las casillas y separadores de manera indicada y poniendo la información de la partida (estado de la banca y de los jugadores) en la parte derecha del tablero

--- Python/16 Treball en equip/Monopoly.py
+++ Python/16 Treball en equip/Monopoly.py
@@ -1,5 +1,8 @@
 import sys
 import os
+from colorama import just_fix_windows_console # Paquete para que la terminal de Windows entienda los caràcteres ANSI y podamos mover el cursor a la posición que deseemos 
+
+just_fix_windows_console()
 
 MIN_DINERS_BANCA = 500000
 
@@ -12,10 +15,12 @@ casella_mesures = {
 tauler_mesures = {
     "ampla_total": 64,
     "ampla_partida": 10,
-    "alt": 7
+    "files": 7,
+    "columnes": 7
 }
 jugadors = {
     "Vermell": {
+        "nom": "Vermell",
         "icona": "V",
         "diners": 0,
         "posicio": [],
@@ -24,28 +29,31 @@ jugadors = {
         "cartes": []
     },
     "Groc": {
+        "nom": "Groc",
         "icona": "G",
         "diners": 0,
         "posicio": [],
-        "propietats": [],
+        "propietats": ["Gracia", "Pl Cat"],
         "es_preso": False,
         "cartes": []
     },
     "Taronja": {
+        "nom": "Taronja",
         "icona": "T",
         "diners": 0,
         "posicio": [],
-        "propietats": [],
+        "propietats": ["Aribau"],
         "es_preso": False,
         "cartes": []
     },
     "Blau": {
+        "nom": "Blau",
         "icona": "B",
         "diners": 0,
         "posicio": [],
         "propietats": [],
         "es_preso": False,
-        "cartes": []
+        "cartes": ["Sortir preso"]
     }
 }
 
@@ -60,11 +68,21 @@ caselles = {
     },
 }
 
+posicions_caselles_files = [1, 5, 8, 11, 14, 17, 19]
+posicions_caselles_columnes = [0, 10, 19, 28, 37, 45, 55]
+posicions_separadors = [[0, 4], [0, 22]]
+posicions_informacio = [[66, 1], [66, 4], [66, 9], [66, 14], [66, 19]]
+posicio_estat = [5, 11]
+
 def clearScreen():
     if os.name == 'nt':     # Si estàs a Windows
         os.system('cls')
     else:                   # Si estàs a Linux o macOS
         os.system('clear')
+
+def mou_cursor(x, y):
+    sys.stdout.write(f"\033[{y};{x}H")
+    sys.stdout.flush()
 
 def afegir_diners_banca():
     # Añadimos dinero a la banca cuando esta no disponga de suficiente dinero (1.000.000€)
@@ -101,24 +119,30 @@ def crea_casella(nom, nom_acortat, jugadors, posicio, es_especial, opcions_espec
     # Añadimos la casilla generada al tablero
     tauler[nom] = casella
 
-def imprimeix_casella_vertical(nom, cases, hotels, jugadors, fila):
+def imprimeix_separador():
+    amplada = casella_mesures["ampla"]
+    print(f"+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+{"".ljust(amplada, "-")}+")
+
+def imprimeix_casella_vertical(nom, cases, hotels, jugadors, posicio):
     amplada = casella_mesures["ampla"]
     final_linia = "|"
     separador = f"+{"".ljust(amplada, "-")}+"
 
-    
     string_final = f"{final_linia}"
     if cases > 0:
         amplada -= 1
         final_linia = f"{cases}C"
 
-    string_final += f"{nom.ljust(amplada)}{final_linia}\n"
+    string_final += f"{nom.ljust(amplada)}{final_linia}"
+    mou_cursor(posicio[1], posicio[0])
+    print(string_final)
+
     amplada = casella_mesures["ampla"]
     final_linia = "|"
     if hotels > 0:
         amplada -= 1
         final_linia = f"{hotels}H"
-    string_final += "|"
+    string_final = "|"
     if len(jugadors) != 0:
         string_jugadors = ""
         for jugador in jugadors:
@@ -127,13 +151,15 @@ def imprimeix_casella_vertical(nom, cases, hotels, jugadors, fila):
     else:
         string_final += f"{"".ljust(amplada)}"
     string_final += f"{final_linia}"
-    
-    if fila != 1:
-        string_final += f"\n{separador}"
-
+    mou_cursor(posicio[1], posicio[0] + 1)
     print(string_final)
+    
+    if posicio[0] != 17:
+        string_final = f"{separador}"
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(string_final)
 
-def imprimeix_casella_horizontal(nom, cases, hotels, jugadors, fila):
+def imprimeix_casella_horizontal(nom, cases, hotels, jugadors, posicio):
     primera_linia = "+"
     if cases > 0:
         primera_linia += f"{"".ljust(4, "-")}{cases}C"
@@ -141,13 +167,12 @@ def imprimeix_casella_horizontal(nom, cases, hotels, jugadors, fila):
         primera_linia += f"".ljust(6, "-")
 
     if hotels > 0:
-        primera_linia += f"{hotels}H+"
+        primera_linia += f"{hotels}H"
     else:
-        primera_linia += f"".ljust(2, "-") + "+"
+        primera_linia += f"".ljust(2, "-")
     
     amplada = casella_mesures["ampla"]
     final_linia = "|"
-    separador = f"+{"".ljust(amplada, "-")}+"
     if len(jugadors) != 0:
             string_jugadors = ""
             for jugador in jugadors:
@@ -156,15 +181,26 @@ def imprimeix_casella_horizontal(nom, cases, hotels, jugadors, fila):
     else:
         string_jugadors = "".ljust(amplada)
 
-    if fila == 0:
-        string_final = f"{primera_linia}\n{final_linia}{string_jugadors}"
-        string_final += final_linia + "\n"
-        string_final += f"|{nom.ljust(amplada)}{final_linia}"
-    elif fila == tauler_mesures["alt"] - 1:
-        string_final = f"{primera_linia}\n{final_linia}{nom.ljust(amplada)}{final_linia}\n"
-        string_final += f"|{string_jugadors}{final_linia}"
+    if posicio[0] == 19:
+        mou_cursor(posicio[1], posicio[0])
+        print(primera_linia)
 
-    print(string_final)
+        mou_cursor(posicio[1], posicio[0] + 1)
+        print(f"{final_linia}{string_jugadors}{final_linia}")
+
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(f"{final_linia}{nom.ljust(amplada)}{final_linia}")
+
+    elif posicio[0] == 1:
+        mou_cursor(posicio[1], posicio[0])
+        print(primera_linia)
+
+        mou_cursor(posicio[1], posicio[0] + 1)
+        print(f"{final_linia}{nom.ljust(amplada)}{final_linia}")
+
+        mou_cursor(posicio[1], posicio[0] + 2)
+        print(f"{final_linia}{string_jugadors}{final_linia}")
+
 
 def imprimeix_casella(nom, cases, hotels, jugadors, posicio):
     # Imprimimos por pantalla la casilla 
@@ -173,41 +209,79 @@ def imprimeix_casella(nom, cases, hotels, jugadors, posicio):
     #   - Impresión del número de casas y hoteles
     #   - Impresión de los jugadores en la casilla
     # Miramos si la casilla debe tener la información en horizontal o en vertical
-    if posicio[0] != 0 and posicio[0] != tauler_mesures["alt"] - 1:
-        imprimeix_casella_vertical(nom, cases, hotels, jugadors, posicio[0])
+    if posicio[0] != 1 and posicio[0] != 19:
+        imprimeix_casella_vertical(nom, cases, hotels, jugadors, posicio)
     else:
-        imprimeix_casella_horizontal(nom, cases, hotels, jugadors, posicio[0])
+        imprimeix_casella_horizontal(nom, cases, hotels, jugadors, posicio)
 
 def imprimeix_fila(fila_caselles):
     # Recibimos una fila del tablero.
     # Iteramos por la fila e imprimimos cada una de las casillas de la fila
-    if len(fila_caselles) == 2:
-        pass
-    else:
-        pass
+    for casella in fila_caselles:
+            imprimeix_casella(casella["nom_acortat"], casella["cases"], casella["hotels"], casella["jugadors"], casella["posicio"])
 
 def imprimeix_taula(tauler):
     # Imprimimos del tablero, llamando cada de las fila con los separadores
-    pass
+    for index in posicions_caselles_files:
+        fila = list(filter(lambda casella: casella["posicio"][0] == index, tauler))
+        imprimeix_fila(fila)
+    
+    mou_cursor(posicions_separadors[0][0], posicions_separadors[0][1])
+    imprimeix_separador()
+    mou_cursor(posicions_separadors[1][0], posicions_separadors[1][1])
+    imprimeix_separador()
 
 def imprimeix_informacio_banca(banca):
     # Imprimimos la información de la banca
     #   Banca:
     #   Diners: 1838734
-    pass
+    mou_cursor(posicions_informacio[0][0], posicions_informacio[0][1])
+    print("Banca: ")
+    mou_cursor(posicions_informacio[0][0], posicions_informacio[0][1] + 1)
+    print(f"Diners: {banca}")
 
-def imprimeix_informacio_jugador(jugador):
+def imprimeix_informacio_jugador(index, jugador):
     # Imprimimos la información de la banca
     #   Jugador Groc:
     #   Carrers: 2834
     #   Diners: 1838734
     #   Especial: (res) "Nombre de las cartas especiales"
-    pass
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1])
+    print(f"Jugador {jugador["nom"]}: ")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 1)
+    print(f"Carrers: ", end="")
+    longitud = len(jugador["propietats"])
+    if longitud != 0:
+        for index_, propietat in enumerate(jugador["propietats"]):
+            print(f"{propietat}", end="")
+            if index_ != longitud - 1:
+                print(", ", end="")
+
+    else:
+        print("(res)")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 2)
+    print(f"Diners: {jugador["diners"]} ")
+
+    mou_cursor(posicions_informacio[index][0], posicions_informacio[index][1] + 3)
+    print(f"Especial: ", end="")
+    longitud = len(jugador["cartes"])
+    if longitud != 0:
+        for index_, carta in enumerate(jugador["cartes"]):
+            print(f"{carta}", end="")
+            if index_ != longitud - 1:
+                print(", ", end="")
+    else:
+        print("(res)")  
 
 def imprimeix_informacio(banca, jugadors):
     # Gestiona la impresión a la izquierda del tablero
     # ¡MIRAR COMO CAMBIAR LA POSICIÓN DEL PUNTERO DE CONSOLA!
-    pass
+    imprimeix_informacio_banca(banca)
+
+    for index, jugador in enumerate(jugadors.values()):
+        imprimeix_informacio_jugador(index + 1, jugador)
 
 def mira_propietari(casella_propietari, jugador):
     # Devuelva si el jugador es el propietario de la casilla o no
@@ -247,21 +321,60 @@ def gestiona_accio_usuari():
     # Añadir dentro de acciones, que se deberán imprimir al acabar su turno
     pass
 
-def mou_cursor(x, y):
-    sys.stdout.write(f"\033[{y};{x}H")
-    sys.stdout.flush()
+clearScreen()
+caselles = [
+    {
+        "nom_acortat": "Gracia",
+        "cases": 2,
+        "hotels": 0,
+        "jugadors": ["V"],
+        "posicio": [19, 10]
+    },
+    {
+        "nom_acortat": "Marina",
+        "cases": 1,
+        "hotels": 1,
+        "jugadors": [],
+        "posicio": [19, 0]
+    },
+    {
+        "nom_acortat": "Aribau",
+        "cases": 0,
+        "hotels": 2,
+        "jugadors": ["VGB"],
+        "posicio": [17, 0]
+    },
+    {
+        "nom_acortat": "Mallorca",
+        "cases": 0,
+        "hotels": 0,
+        "jugadors": ["B"],
+        "posicio": [5, 0]
+    },
+    {
+        "nom_acortat": "Arago",
+        "cases": 1,
+        "hotels": 1,
+        "jugadors": ["T"],
+        "posicio": [1, 0]
+    },
+    {
+        "nom_acortat": "Pl Cat",
+        "cases": 4,
+        "hotels": 2,
+        "jugadors": ["TB"],
+        "posicio": [1, 55]
+    },
+    {
+        "nom_acortat": "Sants",
+        "cases": 2,
+        "hotels": 0,
+        "jugadors": [""],
+        "posicio": [5, 55]
+    },
+]
 
 clearScreen()
-imprimeix_casella("Marina", 1, 2, ["V"], [6, 10])
-print(f"+{"".ljust(8, "-")}+")
-mou_cursor(11, 0)
-imprimeix_casella("Sants", 3, 2, [], [0, 9])
-mou_cursor(11, 4)
-print(f"+{"".ljust(8, "-")}+")
-mou_cursor(0,5)
-imprimeix_casella("Gracia", 0, 0, [], [2, 9])
-mou_cursor(0, 8)
-imprimeix_casella("Gracia", 1, 3, ["V"], [1, 9])
-mou_cursor(0, 10)
-imprimeix_casella("Marina", 0, 2, [], [0, 9])
-print(f"+{"".ljust(8, "-")}+")
+imprimeix_taula(caselles)
+imprimeix_informacio(banca, jugadors)
+mou_cursor(0, 25)
