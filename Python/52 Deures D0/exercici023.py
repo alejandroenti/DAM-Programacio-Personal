ddf9b38da7a64b0aab564056672597bfe4d43671
Alejandro López <116338818+alejandroenti@users.noreply.github.com>
2024-10-30 15:28:55 +0100

    Generando archivos .py para realizar los ejercicios con la plantilla de los resuletos

---
+++ Python/52 Deures D0/exercici023.py
@@ -0,0 +1,64 @@
+import math
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import pygame
+import sys
+import utils
+
+# Definir colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (50, 120, 200)
+BROWN = (165, 42, 42)
+YELLOW = (255, 255, 0)
+GRAY = (169, 169, 169) 
+ORANGE = (255, 165, 0)
+GOLD = (255, 215, 0)
+RED = (255, 69, 0) 
+
+pygame.init()
+clock = pygame.time.Clock()
+
+# Definir la finestra
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption('Window Title')
+
+# Bucle de l'aplicació
+def main():
+    is_looping = True
+
+    while is_looping:
+        is_looping = app_events()
+        app_run()
+        app_draw()
+
+        clock.tick(60) # Limitar a 60 FPS
+
+    # Fora del bucle, tancar l'aplicació
+    pygame.quit()
+    sys.exit()
+
+# Gestionar events
+def app_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Botó tancar finestra
+            return False
+    return True
+
+# Fer càlculs
+def app_run():
+    pass
+
+# Dibuixar
+def app_draw():
+    global sun, planets
+    
+    screen.fill(BLACK)
+    utils.draw_grid(pygame, screen, 50)
+
+    pass
+
+    pygame.display.update()
+
+if __name__ == "__main__":
+    main()
